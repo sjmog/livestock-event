e81dde2b63f0ded0e6c463863f72f94aee718555
Sam Morgan <s_morgan@me.com>
2013-12-18 12:41:48 +0000

    some sexy button effects integrated with AJAX calls from users/new page

--- app/assets/javascripts/views/application_view.js
+++ app/assets/javascripts/views/application_view.js
@@ -1,9 +1,15 @@
 App.ApplicationView = Em.View.extend({
 	templateName: 'application',
 	didInsertElement:function() {
+		var $page = this.$().find('#pageContainer');
+		var $page = this.$().find('#pageContainer');
+		var $pusher = this.$().find('#pusherContainer');
 		$('#resetAuth').click(function() {
 			App.AuthManager.reset();
-		})
+		});
+		Ember.run.later(function() {
+		$page.addClass('in');
+		}, 1);
 	},
 })
   
