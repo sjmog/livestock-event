c53dad049f90a05a49a2cef1ac1a6bff3ea5f512
Sam Morgan <s_morgan@me.com>
2014-02-12 13:39:21 +0000

    changed SMTP settings to custom livestockevent gmail

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => 587,
 	:domain => "gmail.com",
-	:user_name => "sam@ghostds.com",
-	:password => "ace55chopper",
+	:user_name => "livestockevent@gmail.com",
+	:password => "ForAllTheCows123",
 	:authentication => "plain",
 	:enable_starttls_auto => true
 }
