c8cf76e39b2a6132dc012bbdace0bb2de9310082
Sam Morgan <s_morgan@me.com>
2014-04-02 17:59:46 +0100

    split style into two different files to avoid IE selector limit

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module EmberAuthRailsDemo
 
     #compile multiple sets of manifests
 
-    config.assets.precompile += %w( admin.js admin.css )
+    config.assets.precompile += %w( admin.js admin.css applicationextra.css )
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
